863ac25c5fb870ba6830df5640cde43124fd1346
Vikas Puri <prvks09@gmail.com>
2026-02-20 13:19:58 +0530

    Add to cart click project done

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -45,14 +45,14 @@ productsHTML += `<div class="product-container">
     Added
   </div>
 
-  <button class="add-to-cart-button button-primary">Add to Cart</button>
+  <button class="add-to-cart-button js-add-to-cart-button button-primary" data-product-name = "${product.name}" data-product-id = "${product.id}">Add to Cart</button>
   </div>`;
 
 
 
 });
 
-console.log(productsHTML);
+// console.log(productsHTML);
 /*
 document.querySelector('.js-products-grid').append(productsHTML); // This will not work 
 */
@@ -62,3 +62,66 @@ document.querySelector('.js-products-grid').innerHTML += productsHTML;  // Use t
 const container = document.querySelector('.js-products-grid'); //refer 10:31
 container.insertAdjacentHTML('beforeend', productsHTML);
 */
+
+document.querySelectorAll('.js-add-to-cart-button').forEach((button) => {
+  button.addEventListener('click', () => {
+    
+    const productId = button.dataset.productId;
+    // const productName = button.dataset.productName;
+    // cart.push({
+    //   productName,
+    //   quantity: 1,
+    
+    // });
+
+    console.log("button clicked",productId);
+
+    let matchingItem;
+
+    cart.forEach((item) => {
+      console.log("Inside for each");
+
+      if(productId === item.productId){
+        matchingItem = item;
+      }
+
+    });
+
+     if(matchingItem) {
+        console.log("matching item Adding Quantity");
+        matchingItem.quantity += 1;
+      } else {
+         console.log("NO matching item Adding Product + Quantity");
+
+        console.log("pushing new item");
+
+        cart.push(
+          {
+            productId: productId,
+            // productName: productName,
+            quantity: 1
+          }
+
+        );
+      }
+
+
+    console.log(cart);
+  });
+
+
+
+});
+
+
+
+// .addEventListener('click', () => {
+
+//   document.querySelector('.cart-quantity').Tex += 1;
+
+//   document.querySelector('.product-quantity-container')
+
+
+
+
+// });
